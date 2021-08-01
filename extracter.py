38136784ee2347208d7ece7f535a6caaf0622f54
Madlumi <61489225+Madlumi@users.noreply.github.com>
2021-08-01 11:32:07 +0300

    reaoragnized to use more functions and stuff

--- extracter.py
+++ extracter.py
@@ -22,102 +22,86 @@ def sort_nicely(l):
     """
     l.sort(key=alphanum_key)
 
-#ffmpeg output function
+#ffmpeg output function for converting to video
 def output(fps,pre,oFol,oName,bitrate):
     ffmpeg_path= 'ffmpeg'
     cmd = ffmpeg_path+' -y -framerate '+str(fps)+' -i '+oFol+'\\' +pre+r'_%05d.png' + ' -b '+str(bitrate)+' '+oName
     print(cmd)
     return subprocess.check_output(cmd, shell=True)
 
+#Extract files
+def extract(iFol,oFol,pre,repeats):
+    if not os.path.exists(oFol):
+        os.makedirs(oFol)
+    #get file list
+    iii = [x[0] for x in os.walk(iFol)]
+    sort_nicely(iii)
+    #export loop
+    i = 0
+    for iiii in range(0,repeats):
+        for f in iii:
+            #copy files, only supports png, jpg
+            for file in os.listdir(f):
+                if file.endswith(".png"):
+                    fn= (pre+"_"+(f'{i:05d}')+".png")
+                    print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
+                    copyfile(os.path.join(f, file),os.path.join(oFol, fn))
+                
+                    i=i+1
 
+                if file.endswith(".jpg"):
+                    fn= (pre+"_"+(f'{i:05d}')+".jpg")
+                    print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
+                    copyfile(os.path.join(f, file),os.path.join(oFol, fn))
+                
+                    i=i+1
+    return 0
 
-#file paths--------------------------------------------------------
-iFol=r'C:\Users\user\Pictures\inputimagefolder'
-oFol=r'C:\Users\user\Pictures\outputimagefolder'
-pre='file'
+#extract files and export video
+def runExtractor(iFol,oFol,pre,repeats,export,fps,oVid,oVidFol,bitrate,deleteImg):
+    if(extract(iFol,oFol,pre,repeats)!=0):
+        print("Error extracting images")
+        return "extractionError"
+    #---------------------------
+    if(export==True):
+        if not os.path.exists(oVidFol):
+            os.makedirs(oVidFol)
+        o = output(fps,pre,oFol,oVidFol+'\\'+oVid,bitrate)
+        if(o==b''):
+            print("successful export!")
+        else:
+            print(o)
+            return "exportError : "+o
+        #delete images after done
+        if(deleteImg):
+            try:
+                shutil.rmtree(oFol)
+            except OSError as e:
+                print ("imgDeleteError : %s - %s." % (e.filename, e.strerror))
+                return "imgDeleteError : %s - %s." % (e.filename, e.strerror)
+    return 0
 
-repeats = 1
+                
+if __name__ == "__main__":
+    #file paths--------------------------------------------------------
+    iFol=r'C:\Users\user\Pictures\inputimagefolder'
+    oFol=r'C:\Users\user\Pictures\outputimagefolder'
 
-#export options, requres ffmpeg-----------------------------------
-export=True
-fps=12
-oVid="videoname.avi"
-oVidFol=r'C:\Users\user\videos'
-bitrate='51200k'
+    
+    pre='file'
 
-#delete images after done if exporting(note this will require oFol and oVidFol to be different)
-deleteImg=True
+    repeats = 1
 
-#Extract-----------------------------------------------------------
-if not os.path.exists(oFol):
-    os.makedirs(oFol)
-#get file list
-iii = [x[0] for x in os.walk(iFol)]
-sort_nicely(iii)
-#export loop
-i = 0
-for iiii in range(0,repeats):
-    for f in iii:
-        #copy files, only supports png, jpg
-        for file in os.listdir(f):
-            if file.endswith(".png"):
-                fn= (pre+"_"+(f'{i:05d}')+".png")
-                print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
-                copyfile(os.path.join(f, file),os.path.join(oFol, fn))
-            
-                i=i+1
+    #export options, requres ffmpeg-----------------------------------
+    export=True
+    fps=12
+    oVid="videoname.avi"
+    oVidFol=r':\Users\user\Videos'
+    bitrate='51200k'
 
-            if file.endswith(".jpg"):
-                fn= (pre+"_"+(f'{i:05d}')+".jpg")
-                print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
-                copyfile(os.path.join(f, file),os.path.join(oFol, fn))
-            
-                i=i+1
-#Export Video----------------------------------------------------
-#delete images after done if exporting(note this will require oFol and oVidFol to be different)
-deleteImg=True
-
-#Extract-----------------------------------------------------------
-if not os.path.exists(oFol):
-    os.makedirs(oFol)
-#get file list
-iii = [x[0] for x in os.walk(iFol)]
-sort_nicely(iii)
-#export loop
-i = 0
-for iiii in range(0,repeats):
-    for f in iii:
-        #copy files, only supports png, jpg
-        for file in os.listdir(f):
-            if file.endswith(".png"):
-                fn= (pre+"_"+(f'{i:05d}')+".png")
-                print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
-                copyfile(os.path.join(f, file),os.path.join(oFol, fn))
-            
-                i=i+1
-
-            if file.endswith(".jpg"):
-                fn= (pre+"_"+(f'{i:05d}')+".jpg")
-                print(os.path.join(f, file)+"  "+os.path.join(oFol, fn))
-                copyfile(os.path.join(f, file),os.path.join(oFol, fn))
-            
-                i=i+1
-#Export Video----------------------------------------------------
-if(export==True):
-    if not os.path.exists(oVidFol):
-        os.makedirs(oVidFol)
-    o = output(fps,pre,oFol,oVidFol+'\\'+oVid,bitrate)
-    if(o==b''):
-        print("successful export!")
-    else:
-        print(o)
-    #delete images after done
-    if(deleteImg):
-        try:
-            shutil.rmtree(oFol)
-        except OSError as e:
-            print ("Error: %s - %s." % (e.filename, e.strerror))
-        
+    #delete images after done if exporting(note this will require oFol and oVidFol to be different)
+    deleteImg=True
+    runExtractor(iFol,oFol,pre,repeats,export,fps,oVid,oVidFol,bitrate,deleteImg)
 
 
 
